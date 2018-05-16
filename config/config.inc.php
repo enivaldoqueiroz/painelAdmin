98fca0303d526ffd3ac2d10ac6c0f668b49b7777
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-05-15 21:35:02 -0300

    Primeiro commit - Painel Admin Resposivo com Bootstrap. Criação de Formulario de Login.

---
+++ config/config.inc.php
@@ -0,0 +1,5 @@
+<?php
+
+/* FRANQUIA DO DO SITE */
+
+define ('TITULO', 'Painel Administrativo');
---
+++ pag/login.php
@@ -0,0 +1,6 @@
+<form method="post" action="exe/exe_login.php">
+    <input type="email" name="user_email">
+    <input type="password" name="user_senha">
+    <button>Efetuar Login</button>
+
+</form>
