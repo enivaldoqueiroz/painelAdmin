2f10916bb78ea7117b98da2cdb96658dae0338f8
Enivaldo Queiroz <enivaldo.jecq@gmail.com>
2018-05-15 22:54:05 -0300

    Melhorando a aparencia do formurio de login com o bootstrap.

--- pag/login.php
+++ pag/login.php
@@ -1,6 +1,21 @@
-<form method="post" action="exe/exe_login.php">
-    <input type="email" name="user_email">
-    <input type="password" name="user_senha">
-    <button>Efetuar Login</button>
+<div class="row">
+  <div class="col-md-4"></div>
+  <div class="col-md-4">
+  
+    <form method="post" action="exe/exe_login.php">
+        <div class="form-group">
+        <input class="form-control" type="email" name="user_email">
+        </div>
+        
+        <div class="form-group">
+        <input class="form-control" type="password" name="user_senha">
+        </div>
 
-</form>
+        
+        <button class="btn btn-primary btn-block"><i class="glyphicon glyphicon-log-in"></i>Efetuar Login</button>
+       
+    </form>
+  
+  </div>
+  <div class="col-md-4"></div>
+</div>
